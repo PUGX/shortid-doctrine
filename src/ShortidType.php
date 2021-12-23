42f87bc23e05bc45f7ab9f0f1a45d95723b97af8
spackmat <gregor@der-meyer.de>
2021-12-23 12:53:01 +0100

    Resolve three of four requested changes
    The fourth cannot be changed as it would raise a fatal error.

--- src/ShortidType.php
+++ src/ShortidType.php
@@ -1,7 +1,4 @@
 <?php
-
-declare(strict_types=1);
-
 namespace PUGX\Shortid\Doctrine;
 
 use Doctrine\DBAL\Platforms\AbstractPlatform;
@@ -20,9 +17,6 @@ final class ShortidType extends Type
 {
     public const NAME = 'shortid';
 
-    /**
-     * {@inheritdoc}
-     */
     public function getSQLDeclaration(array $column, AbstractPlatform $platform): string
     {
         $length = $column['length'] ?? 7;
@@ -36,47 +30,35 @@ final class ShortidType extends Type
         return $platform->getVarcharTypeDeclarationSQL($field).' '.$platform->getColumnCollationDeclarationSQL('utf8_bin');
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function convertToPHPValue($value, AbstractPlatform $platform): ?Shortid
     {
         if (empty($value)) {
             return null;
         }
-        if (ShortId::isValid((string) $value)) {
+        if (ShortId::isValid($value)) {
             return new Shortid($value);
         }
 
         throw ConversionException::conversionFailed($value, self::NAME);
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function convertToDatabaseValue($value, AbstractPlatform $platform): ?string
     {
         if (empty($value)) {
             return null;
         }
-        if ($value instanceof ShortId || ShortId::isValid((string) $value)) {
+        if ($value instanceof ShortId || ShortId::isValid($value)) {
             return (string) $value;
         }
 
         throw ConversionException::conversionFailed($value, self::NAME);
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function getName(): string
     {
         return self::NAME;
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function requiresSQLCommentHint(AbstractPlatform $platform): bool
     {
         return true;
